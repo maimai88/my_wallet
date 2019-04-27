24d7b1bd52ec830a28669dbb89fea5df89d737ff
Suesi Tran <tranhathuy@gmail.com>
2019-04-27 17:38:04 +1000

    Remove primary_secondary_progress_bar code
    Change to use primary_secondary_progress_bar plugin

--- lib/app_material.dart
+++ lib/app_material.dart
@@ -13,4 +13,4 @@ export 'package:my_wallet/widget/page_route.dart';
 export 'package:my_wallet/widget/enter_name_view.dart';
 export 'package:my_wallet/widget/data_row.dart';
 export 'package:my_wallet/widget/error_page.dart';
-export 'package:my_wallet/widget/primary_secondary_slider_view.dart';
+export 'package:primary_secondary_progress_bar/primary_secondary_progress_bar.dart';

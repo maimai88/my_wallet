d2de7025c74d54121aadfffa351510ce544b4d82
suesi tran <tranhathuy@gmail.com>
2019-05-06 12:58:35 +1000

    Add create category route in Budget

--- lib/ui/dashboard/dashbard_budget_navigator.dart
+++ lib/ui/dashboard/dashbard_budget_navigator.dart
@@ -4,6 +4,7 @@ import 'package:my_wallet/ui/dashboard/no_transition_page_route.dart';
 
 import 'package:my_wallet/ui/budget/list/presentation/view/list_view.dart';
 import 'package:my_wallet/ui/budget/detail/presentation/view/detail_view.dart';
+import 'package:my_wallet/ui/budget/category/presentation/view/create_category_view.dart';
 
 class BudgetNavigator extends StatelessWidget {
   final GlobalKey<NavigatorState> navigatorKey;
@@ -52,6 +53,21 @@ class BudgetNavigator extends StatelessWidget {
           return BudgetDetail("Budget", categoryId: int.parse(title.replaceFirst("${routes.AddBudget}/", "")), month: day,);
         } catch(e) {}
       } while (false);
+    }
+
+    if(name.startsWith(routes.CreateCategory)) {
+      List<String> splits = name.split("/"); //  "$TransferAccount/from:$accountId/name:$accountName";;
+
+      if(splits.length > 1) {
+        // get category ID:
+        var id = int.parse(splits[1].split(":")[1]);
+        // get category name
+        var accName = splits[2].split(":")[1];
+
+        return CreateCategory(title: "Edit Category", id: id, name: accName);
+      }
+
+      return CreateCategory();
     }
 
     return PlainScaffold(

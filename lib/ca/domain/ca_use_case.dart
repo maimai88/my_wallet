43ce81d8ee0f68abb067024416a6074a08ebc1a0
Suesi Tran <tranhathuy@gmail.com>
2019-05-12 11:21:06 +1000

    Add cancel subscription to clear stream subscription once it's done

--- lib/ca/domain/ca_use_case.dart
+++ lib/ca/domain/ca_use_case.dart
@@ -10,28 +10,42 @@ import 'package:flutter/foundation.dart';
 export 'package:flutter/foundation.dart';
 
 import 'package:flutter/scheduler.dart';
+import 'dart:async';
 
 /// Free flow, use your repo as you wish in your own usecase, and return data as you need
 class CleanArchitectureUseCase<T extends CleanArchitectureRepository> {
   final T repo;
 
   CleanArchitectureUseCase(this.repo);
+  Map<String, StreamSubscription> _streamSubscription = {};
 
   void execute<T>(Future<T> task, onNext<T> next, onError error) {
     if(task != null) {
-      SchedulerBinding.instance.addPostFrameCallback((_) => Observable(Stream.fromFuture(task)).listen((data) => next(data), onError: (e, stacktrace) {
-        debugPrint(stacktrace.toString());
+    final subscriptionKey = DateTime.now().millisecondsSinceEpoch.toRadixString(12);
+      StreamSubscription subscription = Observable.fromFuture(task).listen((data) => next(data), onError: (e, stacktrace) {
+        debugPrintStack(label: "Task failed", maxFrames: 30);
 
-        if (error != null) {
-          if (e is Exception) {
+        if(error != null) {
+          if(e is Exception) {
             error(e);
           } else {
             error(Exception(e.toString()));
           }
         }
-      }));
+      }, onDone: () => clearSubscription(subscriptionKey));
+
+      _streamSubscription.putIfAbsent(subscriptionKey, () => subscription);
     } else {
       debugPrint("task is null: $task ${this.toString()}");
+    }
+  }
+
+  void clearSubscription(String key) {
+    print("clear subscription $key");
+    StreamSubscription subscription = _streamSubscription.remove(key);
+
+    if(subscription != null) {
+      subscription.cancel();
     }
   }
 }
